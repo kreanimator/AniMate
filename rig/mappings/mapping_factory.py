cecd86f9e70924a69378740149a1d0eebf53e033
kreanimator <backin.v@gmail.com>
2025-06-02 13:29:48 +0300

    Fixes

--- rig/mappings/mapping_factory.py
+++ rig/mappings/mapping_factory.py
@@ -1,15 +1,23 @@
 """
 Factory for creating rig mappings.
 """
+import bpy
 from .base_mappings import BaseRigMapping
 from .mixamo_mapping import MixamoMapping
 from .rigify_mapping import RigifyMapping
+from .maya_mapping import MayaMapping
 
 class RigMappingFactory:
     """Factory for creating rig mappings."""
     
-    @staticmethod
-    def create_mapping(rig_type: str) -> BaseRigMapping:
+    _mappings = {
+        'MIXAMO': MixamoMapping,
+        'RIGIFY': RigifyMapping,
+        'MAYA': MayaMapping
+    }
+
+    @classmethod
+    def create_mapping(cls, rig_type: str) -> BaseRigMapping:
         """
         Create a mapping for the specified rig type.
         
@@ -22,18 +30,35 @@ class RigMappingFactory:
         Raises:
             ValueError: If the rig type is not supported
         """
-        mapping_classes = {
-            'MIXAMO': MixamoMapping,
-            'RIGIFY': RigifyMapping,
-            # Add more rig types here
-        }
-        
-        if rig_type not in mapping_classes:
+        if rig_type not in cls._mappings:
             raise ValueError(f"Unsupported rig type: {rig_type}")
-            
-        return mapping_classes[rig_type]()
+        return cls._mappings[rig_type]()
     
-    @staticmethod
-    def get_supported_rig_types() -> list:
-        """Get a list of supported rig types."""
-        return ['MIXAMO', 'RIGIFY']  # Add more as they are implemented 
+    @classmethod
+    def get_supported_rigs(cls):
+        """Get list of supported rig types."""
+        return list(cls._mappings.keys())
+
+    @classmethod
+    def get_bone_hierarchy(cls, rig_type):
+        """Get the expected bone hierarchy for a rig type."""
+        mapping = cls.create_mapping(rig_type)
+        return mapping.get_bone_hierarchy()
+
+    @classmethod
+    def get_pose_mapping(cls, rig_type):
+        """Get the pose mapping for a rig type."""
+        mapping = cls.create_mapping(rig_type)
+        return mapping.get_pose_mapping()
+
+    @classmethod
+    def get_face_mapping(cls, rig_type):
+        """Get the face mapping for a rig type."""
+        mapping = cls.create_mapping(rig_type)
+        return mapping.get_face_mapping()
+
+    @classmethod
+    def get_hand_mapping(cls, rig_type):
+        """Get the hand mapping for a rig type."""
+        mapping = cls.create_mapping(rig_type)
+        return mapping.get_hand_mapping() 
--- rig/mappings/mixamo_mapping.py
+++ rig/mappings/mixamo_mapping.py
@@ -4,96 +4,10 @@ Mixamo rig mapping implementation.
 from .base_mappings import BaseRigMapping
 
 class MixamoMapping(BaseRigMapping):
-    """Mapping implementation for Mixamo rigs."""
-    
-    def get_pose_mapping(self):
-        # MediaPipe Pose indices: https://google.github.io/mediapipe/solutions/pose.html#pose-landmark-model-blazepose-ghum-3d
-        return {
-            # Hips and Spine
-            'Hips': [23, 24],  # left_hip to right_hip
-            'Spine': [23, 11],  # left_hip to left_shoulder
-            'Spine1': [11, 12],  # left_shoulder to right_shoulder
-            'Spine2': [12, 0],  # right_shoulder to nose (approximate upper spine)
-            'Neck': [0, 1],  # nose to left_eye_inner (approximate neck)
-            'Head': [0, 8],  # nose to forehead (approximate)
-            'HeadTop_End': [8, 8],  # forehead (no landmark for head top, so repeat)
+    """Mapping for Mixamo rigs."""
 
-            # Right Arm
-            'RightShoulder': [12, 14],  # right_shoulder to right_elbow
-            'RightArm': [14, 16],  # right_elbow to right_wrist
-            'RightForeArm': [14, 16],  # same as above (no separate landmark)
-            'RightHand': [16, 16],  # right_wrist (single point)
-
-            # Left Arm
-            'LeftShoulder': [11, 13],  # left_shoulder to left_elbow
-            'LeftArm': [13, 15],  # left_elbow to left_wrist
-            'LeftForeArm': [13, 15],  # same as above
-            'LeftHand': [15, 15],  # left_wrist
-
-            # Right Leg
-            'RightUpLeg': [24, 26],  # right_hip to right_knee
-            'RightLeg': [26, 28],  # right_knee to right_ankle
-            'RightFoot': [28, 32],  # right_ankle to right_heel
-            'RightToeBase': [32, 32],  # right_heel (no separate toe landmark)
-            'RightToe_End': [32, 32],  # right_heel (repeat, or remove if not needed)
-
-            # Left Leg
-            'LeftUpLeg': [23, 25],  # left_hip to left_knee
-            'LeftLeg': [25, 27],  # left_knee to left_ankle
-            'LeftFoot': [27, 31],  # left_ankle to left_heel
-            'LeftToeBase': [31, 31],  # left_heel
-            'LeftToe_End': [31, 31],  # left_heel
-        }
-    
-    def get_hand_mapping(self):
-        # MediaPipe Hands indices: https://google.github.io/mediapipe/solutions/hands.html#hand-landmark-model
-        # Thumb: 1-2-3-4, Index: 5-6-7-8, Middle: 9-10-11-12, Ring: 13-14-15-16, Pinky: 17-18-19-20
-        # Wrist: 0
-        return {
-            # Right hand (use same for left, code will swap as needed)
-            'RightHand': [0, 0],  # wrist
-            'RightHandThumb1': [1, 2],
-            'RightHandThumb2': [2, 3],
-            'RightHandThumb3': [3, 4],
-            'RightHandIndex1': [5, 6],
-            'RightHandIndex2': [6, 7],
-            'RightHandIndex3': [7, 8],
-            'RightHandMiddle1': [9, 10],
-            'RightHandMiddle2': [10, 11],
-            'RightHandMiddle3': [11, 12],
-            'RightHandRing1': [13, 14],
-            'RightHandRing2': [14, 15],
-            'RightHandRing3': [15, 16],
-            'RightHandPinky1': [17, 18],
-            'RightHandPinky2': [18, 19],
-            'RightHandPinky3': [19, 20],
-            # Left hand (same indices, code will handle side)
-            'LeftHand': [0, 0],
-            'LeftHandThumb1': [1, 2],
-            'LeftHandThumb2': [2, 3],
-            'LeftHandThumb3': [3, 4],
-            'LeftHandIndex1': [5, 6],
-            'LeftHandIndex2': [6, 7],
-            'LeftHandIndex3': [7, 8],
-            'LeftHandMiddle1': [9, 10],
-            'LeftHandMiddle2': [10, 11],
-            'LeftHandMiddle3': [11, 12],
-            'LeftHandRing1': [13, 14],
-            'LeftHandRing2': [14, 15],
-            'LeftHandRing3': [15, 16],
-            'LeftHandPinky1': [17, 18],
-            'LeftHandPinky2': [18, 19],
-            'LeftHandPinky3': [19, 20],
-        }
-    
-    def get_face_mapping(self):
-        return {
-            'Jaw': [152, 175],  # Jaw movement
-            'LeftEye': [33, 133],  # Left eye movement
-            'RightEye': [362, 263],  # Right eye movement
-        }
-    
     def get_bone_hierarchy(self):
+        """Get the expected bone hierarchy for Mixamo rigs."""
         return {
             "Hips": {
                 "Spine": {
@@ -103,88 +17,70 @@ class MixamoMapping(BaseRigMapping):
                                 "Head": {
                                     "HeadTop_End": {}
                                 }
-                            }
-                        }
-                    }
-                },
-                "LeftUpLeg": {
-                    "LeftLeg": {
-                        "LeftFoot": {
-                            "LeftToeBase": {
-                                "LeftToe_End": {}
-                            }
-                        }
-                    }
-                },
-                "RightUpLeg": {
-                    "RightLeg": {
-                        "RightFoot": {
-                            "RightToeBase": {
-                                "RightToe_End": {}
-                            }
-                        }
-                    }
-                },
-                "LeftShoulder": {
-                    "LeftArm": {
-                        "LeftForeArm": {
-                            "LeftHand": {
-                                "LeftHandThumb1": {
-                                    "LeftHandThumb2": {
-                                        "LeftHandThumb3": {}
-                                    }
-                                },
-                                "LeftHandIndex1": {
-                                    "LeftHandIndex2": {
-                                        "LeftHandIndex3": {}
-                                    }
-                                },
-                                "LeftHandMiddle1": {
-                                    "LeftHandMiddle2": {
-                                        "LeftHandMiddle3": {}
-                                    }
-                                },
-                                "LeftHandRing1": {
-                                    "LeftHandRing2": {
-                                        "LeftHandRing3": {}
-                                    }
-                                },
-                                "LeftHandPinky1": {
-                                    "LeftHandPinky2": {
-                                        "LeftHandPinky3": {}
+                            },
+                            "RightShoulder": {
+                                "RightArm": {
+                                    "RightForeArm": {
+                                        "RightHand": {
+                                            "RightHandThumb1": {
+                                                "RightHandThumb2": {
+                                                    "RightHandThumb3": {}
+                                                }
+                                            },
+                                            "RightHandIndex1": {
+                                                "RightHandIndex2": {
+                                                    "RightHandIndex3": {}
+                                                }
+                                            },
+                                            "RightHandMiddle1": {
+                                                "RightHandMiddle2": {
+                                                    "RightHandMiddle3": {}
+                                                }
+                                            },
+                                            "RightHandRing1": {
+                                                "RightHandRing2": {
+                                                    "RightHandRing3": {}
+                                                }
+                                            },
+                                            "RightHandPinky1": {
+                                                "RightHandPinky2": {
+                                                    "RightHandPinky3": {}
+                                                }
+                                            }
+                                        }
                                     }
                                 }
-                            }
-                        }
-                    }
-                },
-                "RightShoulder": {
-                    "RightArm": {
-                        "RightForeArm": {
-                            "RightHand": {
-                                "RightHandThumb1": {
-                                    "RightHandThumb2": {
-                                        "RightHandThumb3": {}
-                                    }
-                                },
-                                "RightHandIndex1": {
-                                    "RightHandIndex2": {
-                                        "RightHandIndex3": {}
-                                    }
-                                },
-                                "RightHandMiddle1": {
-                                    "RightHandMiddle2": {
-                                        "RightHandMiddle3": {}
-                                    }
-                                },
-                                "RightHandRing1": {
-                                    "RightHandRing2": {
-                                        "RightHandRing3": {}
-                                    }
-                                },
-                                "RightHandPinky1": {
-                                    "RightHandPinky2": {
-                                        "RightHandPinky3": {}
+                            },
+                            "LeftShoulder": {
+                                "LeftArm": {
+                                    "LeftForeArm": {
+                                        "LeftHand": {
+                                            "LeftHandThumb1": {
+                                                "LeftHandThumb2": {
+                                                    "LeftHandThumb3": {}
+                                                }
+                                            },
+                                            "LeftHandIndex1": {
+                                                "LeftHandIndex2": {
+                                                    "LeftHandIndex3": {}
+                                                }
+                                            },
+                                            "LeftHandMiddle1": {
+                                                "LeftHandMiddle2": {
+                                                    "LeftHandMiddle3": {}
+                                                }
+                                            },
+                                            "LeftHandRing1": {
+                                                "LeftHandRing2": {
+                                                    "LeftHandRing3": {}
+                                                }
+                                            },
+                                            "LeftHandPinky1": {
+                                                "LeftHandPinky2": {
+                                                    "LeftHandPinky3": {}
+                                                }
+                                            }
+                                        }
                                     }
                                 }
                             }
@@ -193,25 +89,100 @@ class MixamoMapping(BaseRigMapping):
                 }
             }
         }
-    
-    def get_bone_rotation_limits(self):
-        """Get the rotation limits for each bone in degrees."""
+
+    def get_pose_mapping(self):
+        """Get the mapping between MediaPipe pose landmarks and bone names."""
         return {
-            'Spine': {'x': (-30, 30), 'y': (-30, 30), 'z': (-30, 30)},
-            'Neck': {'x': (-30, 30), 'y': (-30, 30), 'z': (-30, 30)},
-            'LeftShoulder': {'x': (-90, 90), 'y': (-90, 90), 'z': (-90, 90)},
-            'RightShoulder': {'x': (-90, 90), 'y': (-90, 90), 'z': (-90, 90)},
-            # Add more bone limits as needed
+            "Hips": [23, 24],  # Left and right hip
+            "Spine": [23, 11],  # Hip to mid-spine
+            "Spine1": [11, 12],  # Mid-spine to upper spine
+            "Spine2": [12, 0],  # Upper spine to neck
+            "Neck": [0, 2],  # Neck to head
+            "Head": [2, 8],  # Head to nose
+            "RightShoulder": [11, 12],  # Spine to right shoulder
+            "RightArm": [12, 14],  # Shoulder to elbow
+            "RightForeArm": [14, 16],  # Elbow to wrist
+            "LeftShoulder": [11, 12],  # Spine to left shoulder
+            "LeftArm": [12, 13],  # Shoulder to elbow
+            "LeftForeArm": [13, 15],  # Elbow to wrist
         }
-    
-    def get_bone_scale_factors(self):
-        """Get the scale factors for each bone."""
+
+    def get_face_mapping(self):
+        """Get the mapping between MediaPipe face landmarks and bone names."""
         return {
-            'Spine': 1.0,
-            'Neck': 1.0,
-            'LeftShoulder': 1.0,
-            'RightShoulder': 1.0,
-            # Add more bone scale factors as needed
+            "Head": [0, 1],  # Nose to forehead
+            "Neck": [1, 2],  # Forehead to chin
+        }
+
+    def get_hand_mapping(self):
+        """Get the mapping between MediaPipe hand landmarks and bone names."""
+        return {
+            # Right hand
+            "RightHandThumb1": [0, 1],
+            "RightHandThumb2": [1, 2],
+            "RightHandThumb3": [2, 3],
+            "RightHandIndex1": [0, 5],
+            "RightHandIndex2": [5, 6],
+            "RightHandIndex3": [6, 7],
+            "RightHandMiddle1": [0, 9],
+            "RightHandMiddle2": [9, 10],
+            "RightHandMiddle3": [10, 11],
+            "RightHandRing1": [0, 13],
+            "RightHandRing2": [13, 14],
+            "RightHandRing3": [14, 15],
+            "RightHandPinky1": [0, 17],
+            "RightHandPinky2": [17, 18],
+            "RightHandPinky3": [18, 19],
+            # Left hand
+            "LeftHandThumb1": [0, 1],
+            "LeftHandThumb2": [1, 2],
+            "LeftHandThumb3": [2, 3],
+            "LeftHandIndex1": [0, 5],
+            "LeftHandIndex2": [5, 6],
+            "LeftHandIndex3": [6, 7],
+            "LeftHandMiddle1": [0, 9],
+            "LeftHandMiddle2": [9, 10],
+            "LeftHandMiddle3": [10, 11],
+            "LeftHandRing1": [0, 13],
+            "LeftHandRing2": [13, 14],
+            "LeftHandRing3": [14, 15],
+            "LeftHandPinky1": [0, 17],
+            "LeftHandPinky2": [17, 18],
+            "LeftHandPinky3": [18, 19],
+        }
+
+    def get_bone_rotation_limits(self):
+        """Get rotation limits for each bone."""
+        return {
+            "Hips": {"x": (-45, 45), "y": (-45, 45), "z": (-45, 45)},
+            "Spine": {"x": (-30, 30), "y": (-30, 30), "z": (-30, 30)},
+            "Spine1": {"x": (-30, 30), "y": (-30, 30), "z": (-30, 30)},
+            "Spine2": {"x": (-30, 30), "y": (-30, 30), "z": (-30, 30)},
+            "Neck": {"x": (-45, 45), "y": (-45, 45), "z": (-45, 45)},
+            "Head": {"x": (-45, 45), "y": (-45, 45), "z": (-45, 45)},
+            "RightShoulder": {"x": (-90, 90), "y": (-90, 90), "z": (-90, 90)},
+            "RightArm": {"x": (-90, 90), "y": (-90, 90), "z": (-90, 90)},
+            "RightForeArm": {"x": (-90, 90), "y": (-90, 90), "z": (-90, 90)},
+            "LeftShoulder": {"x": (-90, 90), "y": (-90, 90), "z": (-90, 90)},
+            "LeftArm": {"x": (-90, 90), "y": (-90, 90), "z": (-90, 90)},
+            "LeftForeArm": {"x": (-90, 90), "y": (-90, 90), "z": (-90, 90)},
+        }
+
+    def get_bone_scale_factors(self):
+        """Get scale factors for each bone's rotation."""
+        return {
+            "Hips": 1.0,
+            "Spine": 1.0,
+            "Spine1": 1.0,
+            "Spine2": 1.0,
+            "Neck": 1.0,
+            "Head": 1.0,
+            "RightShoulder": 1.0,
+            "RightArm": 1.0,
+            "RightForeArm": 1.0,
+            "LeftShoulder": 1.0,
+            "LeftArm": 1.0,
+            "LeftForeArm": 1.0,
         }
 
     def get_capabilities(self):
